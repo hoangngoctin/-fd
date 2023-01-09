0498c0ac822cb3f2d0af20d2d651904b23cfaebf
NgocTin <hoangngoctin0302@gmail.com>
2023-01-09 16:54:35 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,13 @@
+
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    prevArrow:
+    '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow:
+    '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>'
+});
