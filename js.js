d18f6e4693eec417bc36f8b1ece1da7f848b365a
NgocTin <hoangngoctin0302@gmail.com>
2023-01-11 00:08:18 +0700

    fix

--- js.js
+++ js.js
@@ -9,5 +9,7 @@ $('.multiple-items').slick({
     prevArrow:
     '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow:
-    '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>'
+    '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>',
+    // appendDots:
+    // ''
 });
